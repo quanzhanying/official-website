aabe44b95a454cb7ee433c0a45e84c59e9cb6502
xdite <xuite.joke@gmail.com>
2016-08-31 19:11:24 +0800

    lightbox

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,4 @@
+class StudentsController < ApplicationController
+  def projects
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get "/overview", to: "welcome#overview"
   get "/syallabus", to: "welcome#syallabus"
   get "/students", to: "welcome#students"
+  get "/projects", to: "students#projects"
   root to: "welcome#index"
 end
