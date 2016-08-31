bc475fdf4c0bfce8bf2bbadf8da27ac9ec0cb995
xdite <xuite.joke@gmail.com>
2016-09-01 02:18:29 +0800

    update all

--- app/helpers/faqs_helper.rb
+++ app/helpers/faqs_helper.rb
@@ -1,2 +1,11 @@
 module FaqsHelper
+  def render_faq_title(faq)
+    faq_class =
+      if faq.is_display
+        ""
+      else
+        "collapsed"
+                     end
+    link_to(faq.title, "#faq-#{faq.id}", class: "accordion-toggle #{faq_class}", data: { toggle: "collapse", parent: "#accordion" })
+  end
 end
