f07f5a27a39339b684a263659b3b5c78c4f66777
xdite <xuite.joke@gmail.com>
2016-08-31 14:35:56 +0800

    overview

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,7 @@
 class WelcomeController < ApplicationController
+  def index
+  end
+
+  def overview
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  get "/overview", to: "welcome#overview"
   root to: "welcome#index"
 end
