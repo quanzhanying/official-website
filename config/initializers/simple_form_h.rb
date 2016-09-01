1922e03aa1306046a5c122e4525c9173c3acb20a
xdite <xuite.joke@gmail.com>
2016-09-01 11:05:06 +0800

    add layout

---
+++ config/initializers/simple_form_h.rb
@@ -0,0 +1,76 @@
+SimpleForm.setup do |config|
+  config.wrappers :horizontal_form, tag: "div", class: "form-group", error_class: "has-error" do |b|
+    b.use :html5
+    b.use :placeholder
+    b.use :label, class: "col-sm-2 control-label"
+
+    b.wrapper tag: "div", class: "col-sm-10" do |ba|
+      ba.use :input, class: "form-control"
+      ba.use :error, wrap_with: { tag: "span", class: "help-block" }
+      ba.use :hint,  wrap_with: { tag: "p", class: "help-block" }
+    end
+  end
+
+  config.wrappers :prepend_append, tag: "div", class: "form-group", error_class: "has-error" do |b|
+    b.use :html5
+    b.use :placeholder
+    b.use :label, class: "col-sm-2 control-label"
+
+    b.wrapper tag: "div", class: "col-sm-10" do |input|
+      input.wrapper tag: "div", class: "input-prepend input-append" do |pa|
+        pa.use :input, class: "form-control"
+      end
+      input.use :error, wrap_with: { tag: "span", class: "help-block" }
+      input.use :hint,  wrap_with: { tag: "p", class: "help-block" }
+    end
+  end
+
+  config.wrappers :horizontal_file_input, tag: "div", class: "form-group", error_class: "has-error" do |b|
+    b.use :html5
+    b.use :placeholder
+    b.use :label, class: "col-sm-2 control-label"
+
+    b.wrapper tag: "div", class: "col-sm-10" do |ba|
+      ba.use :input
+      ba.use :error, wrap_with: { tag: "span", class: "help-block" }
+      ba.use :hint,  wrap_with: { tag: "p", class: "help-block" }
+    end
+  end
+
+  config.wrappers :horizontal_boolean, tag: "div", class: "form-group", error_class: "has-error" do |b|
+    b.use :html5
+    b.use :placeholder
+
+    b.wrapper tag: "div", class: "col-sm-offset-2 col-sm-10" do |wr|
+      wr.wrapper tag: "div", class: "checkbox" do |ba|
+        ba.use :label_input, class: "col-sm-10"
+      end
+
+      wr.use :error, wrap_with: { tag: "span", class: "help-block" }
+      wr.use :hint,  wrap_with: { tag: "p", class: "help-block" }
+    end
+  end
+
+  config.wrappers :horizontal_radio_and_checkboxes, tag: "div", class: "form-group", error_class: "has-error" do |b|
+    b.use :html5
+    b.use :placeholder
+
+    b.use :label, class: "col-sm-2 control-label"
+
+    b.wrapper tag: "div", class: "col-sm-10" do |ba|
+      ba.use :input
+      ba.use :error, wrap_with: { tag: "span", class: "help-block" }
+      ba.use :hint,  wrap_with: { tag: "p", class: "help-block" }
+    end
+  end
+
+  config.wrappers :inline_form, tag: "div", class: "form-group", error_class: "has-error" do |b|
+    b.use :html5
+    b.use :placeholder
+    b.use :label, class: "sr-only"
+
+    b.use :input, class: "form-control"
+    b.use :error, wrap_with: { tag: "span", class: "help-block" }
+    b.use :hint,  wrap_with: { tag: "p", class: "help-block" }
+  end
+end
